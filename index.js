bc01bb83cbb52e40f25e06361bf9c055b4e15f14
HALİL İBRAHİM KÜÇÜKDOĞRU <halilibrahimkucukdogru@posta.mu.edu.tr>
2022-07-07 00:15:35 +0300

    finish pagination

--- index.js
+++ index.js
@@ -34,22 +34,57 @@ app.get("/bestSellers", (req, res) => {
         if (err) {
             console.log(err);
         }
-        else {            
+        else {
             res.json(products);
         }
     });
 })
 
 app.get("/electronic", (req, res) => {
+    const { page = 1, limit = 5 } = req.query;
     Product.find({ productType: "electronic" }, (err, products) => {
         if (err) {
             console.log(err);
         }
-        else {            
-            res.json(products);
+        else {
+            res.json(products)
         }
-    });
+    }).limit(limit * 1)
+        .skip((page - 1) * limit)
+        .exec();
 })
+
+
+const paginate = (model) => {
+    console.log(model);
+    return (req, res, next) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const result = {};
+
+        if (endIndex < model.length) {
+            result.next = {
+                page: page + 1,
+                limit: limit
+            };
+        }
+
+        if (startIndex > 0) {
+            result.previous = {
+                page: page - 1,
+                limit: limit
+            };
+        }
+
+        result.results = model.slice(startIndex, endIndex);
+        res.paginatedResult = result;
+        next();
+    }
+}
+
 
 app.listen(process.env.PORT || port, () => {
     console.log("App is listening!");
