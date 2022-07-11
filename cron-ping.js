59f48a60ddb9d1cbf7c7a130efd60ac45de442b0
HALİL İBRAHİM KÜÇÜKDOĞRU <halilibrahimkucukdogru@posta.mu.edu.tr>
2022-07-11 17:25:10 +0300

    update

--- cron-ping.js
+++ cron-ping.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const scrape = require("./common/scrape");
 const schedule = () => {
+    console.log("Hi");
     cron.schedule('30 17 * * *', function () {        
         console.log("Cron service is running...");
         scrape.getBestSellers();
